973ac6041d69dae123d055ea1bf54d28585320fc
Max Paulo <maxpauloteixeira@gmail.com>
2022-03-28 11:12:51 -0300

    Fix bug to deploy on heroku

--- mirandacarnes/mirandacarnes/wsgi.py
+++ mirandacarnes/mirandacarnes/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mirandacarnes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mirandacarnes.mirandacarnes.settings')
 
 application = get_wsgi_application()
